34805d22e750d2439753e0eff8e5dcacbc5f74eb
DeFUCC <fond@fondfrukt.ru>
2021-03-27 13:27:19 +0300

    may be collections?

--- .vitepress/config/getPages.js
+++ .vitepress/config/getPages.js
@@ -1,18 +1,19 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
+//@ts-ignore
 import { glob } from 'glob'
 
-export default function (dir = '../pages', pattern = '/**/*') {
+export default function (dir = '../pages', pattern = '/**/*.md') {
   const pageDir = path.resolve(__dirname, dir)
 
   const filesList = glob.sync(pageDir + pattern, { nodir: true })
 
   console.info(`Found ${filesList.length} pages`)
-
+  //@ts-ignore
   const filesContent = filesList.map((file) => {
     let info = matter(fs.readFileSync(path.resolve(pageDir, file), 'utf8'))
-
+    //@ts-ignore
     info.url = path.relative(pageDir, file).split('.').shift()
 
     return info
