efa4c58e4a39ef726d983e8e5010b91cd27140c2
davay42 <fond@fondfrukt.ru>
2021-05-11 01:20:34 +0300

    structure

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     Components({
       dirs: [
         '.vitepress/theme/components',
-        'components',
+        '.vitepress/components',
       ],
       extensions: ['vue', 'ts'],
       directoryAsNamespace: true,
@@ -28,7 +28,7 @@ export default defineConfig({
     }),
     WindiCSS({
       scan: {
-        dirs: ['.vitepress', 'components'],
+        dirs: ['.vitepress'],
         include: ['index.md'],
         exclude: ['**/examples/**/*'],
         fileExtensions: ['vue', 'ts'],
