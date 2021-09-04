ee808014767e3ff41c30dcd1cd95084f76de7830
davay42 <fond@fondfrukt.ru>
2021-09-05 00:40:01 +0300

    bump

--- .vitepress/comps/svg/exp.vue
+++ .vitepress/comps/svg/exp.vue
@@ -7,11 +7,7 @@
       :width="100", 
       :height="100",
       fill="green",
-      v-motion,
-      :hovered="{ scale: 1.6, rotate: 10 }",
-      :initial="{ scale: 0.2, rotate: 0 }",
-      :enter="{ scale: 1.2, rotate: 0 }",
-      )
+    )
 </template>
 
 <script setup>
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import Components from 'vite-plugin-components'
+import Components from 'unplugin-vue-components/vite'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 import WindiCSS from 'vite-plugin-windicss'
@@ -28,15 +28,13 @@ export default defineConfig({
       adjustDuplicates:true
     }),
     Components({
-      dirs: [
-        '.vitepress/theme/components',
-        '.vitepress/components',
-      ],
+      dirs: ['.vitepress/theme/components', '.vitepress/comps'],
       extensions: ['vue', 'ts'],
       directoryAsNamespace: true,
       globalNamespaces: ['global'],
-      customLoaderMatcher: id => id.endsWith('.md'),
-      customComponentResolvers: [
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      exclude: [/node_modules/, /\.git/],
+      resolvers: [
         IconsResolver({
           componentPrefix: '',
         }),
