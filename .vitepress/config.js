c337ec259fae1bb4eed300eb58634a9de78288d7
davay <fond@fondfrukt.ru>
2021-12-26 01:49:24 +0300

    bump

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,4 +1,82 @@
-require('esbuild-register')
+import { metaData } from "./config/constants";
+import head from "./config/head";
+//@ts-ignore
+import getTags from "vitepress-tags";
 
-const config = require('./config/index.js')
-module.exports = config.default
+const pages = getTags("./");
+
+export default {
+  title: metaData.title,
+  description: metaData.description,
+  lang: metaData.locale,
+  head,
+  themeConfig: {
+    repo: "DeFUCC/starovdenis.com",
+    logo: "/media/davay.svg",
+    docsRepo: "DeFUCC/starovdenis.com",
+    pages,
+    nav: [
+      {
+        text: "Сотрудничество",
+        link: "/collab/",
+        //@ts-ignore
+        items: pages.collab,
+      },
+      {
+        text: "Творчество",
+        link: "/art/",
+        //@ts-ignore
+        items: pages.art,
+      },
+      {
+        text: "Философия",
+        link: "/philosophy/",
+        //@ts-ignore
+        items: pages.philosophy,
+      },
+      {
+        text: "Контакты",
+        link: "/contact.html",
+        //@ts-ignore
+        items: pages.contact,
+      },
+    ],
+    sidebar: {
+      "/": [
+        {
+          text: "Сотрудничество",
+          link: "/collab/",
+          //@ts-ignore
+          children: pages.collab,
+        },
+        {
+          text: "Творчество",
+          link: "/art/",
+          //@ts-ignore
+          children: pages.art,
+        },
+        {
+          text: "Философия",
+          link: "/philosophy/",
+          //@ts-ignore
+          children: pages.philosophy,
+        },
+        {
+          text: "Контакты",
+          link: "/contact.html",
+          //@ts-ignore
+          children: pages.contact,
+        },
+      ],
+    },
+  },
+  markdown: {
+    config: (md) => {
+      md.use(require("markdown-it-classy"));
+      md.use(require("markdown-it-container"), "card");
+      md.use(require("markdown-it-external-links"), {
+        internalDomains: ["localhost", "starovdenis.com"],
+      });
+    },
+  },
+};
